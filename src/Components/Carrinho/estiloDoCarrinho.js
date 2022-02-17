dd11a88d255a24ae1327dfd709e9f0e06b619efb
formigascegas <ronillson-10@hotmail.com>
2022-02-16 21:18:37 -0300

    Carrinho atualizações.

---
+++ src/Components/Carrinho/estiloDoCarrinho.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const ConjuntoDoCarrinho = styled.div`
+    border: 1px solid black;
+    display: flex;
+    flex-direction: column;
+    padding: 10px;
+    background-color: aliceblue;
+`
